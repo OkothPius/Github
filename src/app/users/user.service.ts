b5cc3041ee7b04418e99c4216675f6afcc370bd4
OkothPius <orukopius8@gmail.com>
2019-03-30 23:58:33 +0300

    Modifying the whole ts

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,27 +1,51 @@
 import { Injectable } from '@angular/core';
-import { Users } from "../users"
 import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class UserService {
 
-  private username: string;
-  private clientid = '1ae2ba5d8370988b390a';
-  private clientsecret = '1ae2ba5d8370988b390a';
-  constructor(private http: Http) {
-    console.log("service is now ready!");
-    this.username = 'OkothPius';
-  }
-  getUserInfo() {
-    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.
-      clientid + "&client_secret=" + this.clientsecret)
-      .map(res => res.json());
-  }
-  getUserRepos() {
-    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.
-      clientid + "&client_secret=" + this.clientsecret)
-      .map(res => res.json());
+  private username= 'OkothPius';
+  private client_id = '1ae2ba5d8370988b390a';
+  private client_secret = 'e4aa440d628189324cda777e0729a07ba52472fb';
+  languageMap = new Map();
 
+  constructor(private _http: Http) {
+    console.log('Github service init');
   }
+
+  getUser() {
+    return this._http.get(`https://api.github.com/users/${this.username}`)
+      .pipe(map(res => res.json()));
+  }
+  getRepos() {
+    return this._http.get(`https://api.github.com/users/${this.username}/repos?per_page=100`)
+      .pipe(map(res => res.json()));
+  }
+  updateUsername(username: string) {
+    this.username = username;
+  }
+  getLanguages(url: string) {
+    return this._http.get(url).pipe(map(res => res.json()));
+  }
+  storeLanguageMap(language: Object) {
+    var languageArray = Object.keys(language);
+    for (var j = 0; j < languageArray.length; j++) {
+      var name = languageArray[j];
+      var value = language[name];
+      console.log(value);
+      if (!this.languageMap.get(name)) {
+        this.languageMap.set(name, value);
+      } else {
+        var tempVal = this.languageMap.get(name);
+        tempVal += value;
+        this.languageMap.set(name, tempVal);
+      }
+      // Do something
+    }
+  }
+  getLanguageMap() {
+    return this.languageMap;
+  }
+
 }
