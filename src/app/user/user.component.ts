4a6d481a1e0e9815d391f018eb53de55587287d5
OkothPius <orukopius8@gmail.com>
2019-03-30 23:56:00 +0300

    Adding to class user.ts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,35 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../user'
-import { UserService } from '../users/user.service';
-import { HttpClient } from '@angular/common/http'
+import {UserService} from '../users/user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  providers: [UserService], //add the providers to the component
-  styleUrls: ['./user.component.css']
+  providers:[UserService]
 })
 export class UserComponent implements OnInit {
-user:any[];
-repos:any[];
+user:any;
+    repos:any;
+    username:string;
+    languages:Object[];
 
-  constructor(private userService: UserService, private http: HttpClient) {
-    this.userService.getUserInfo().subscribe.(user => {
-      console.log(user);
-      this.user=user;
-    });
-    this.userService.getUserRepos().subscribe.(repos =>{
-      console.log(repos);
-      this.repos=repos;
-    })
-  }
+    constructor(private _userService:UserService) {
+        console.log('Github Component init');
 
 
+    }
 
+    search() {
+        this._userService.updateUsername(this.username);
+        this._userService.getUser().subscribe(user=>{
+            this.user = user;
+        });
+
+        this._userService.getRepos().subscribe(repos=>{
+            this.repos = repos;
+            //console.log(repos);
+            this.calculateLanguages(repos);
+        })
+
+    }
+    calculateLanguages(repos){
+        var lang=[];
+        for (let repo of repos){
+            //console.log(repo.languages_url);
+            this._userService.getLanguages(repo.languages_url).subscribe(language=>{
+                lang.push(language);
+                this._userService.storeLanguageMap(language);
+
+            })
+        }
+        this.languages=lang;
+        //str = JSON.stringify(this.languages)
+        console.log(this.languages);
+        console.log(this._userService.getLanguageMap());
+    }
 
   ngOnInit() {
-
-
   }
 
 }
