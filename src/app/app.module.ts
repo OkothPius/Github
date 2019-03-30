c9f87b343ccf4de3a0e7c2065c191baa3ab38e3c
OkothPius <orukopius8@gmail.com>
2019-03-30 16:34:43 +0300

    Modifying module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import { UserDetailsComponent } from './user-details/user-details.component';
-import { StrikethroughDirective } from './strikethrough.directive';
-import { DateCountPipe } from './date-count.pipe';
-import { UserFormComponent } from './user-form/user-form.component';
-import { FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import { UserService } from './users/user.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    UserDetailsComponent,
-    StrikethroughDirective,
-    DateCountPipe,
-    UserFormComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    HttpModule
   ],
-  providers: [], //add service to providers
+  providers: [UserService], //add service to providers
   bootstrap: [AppComponent]
 })
 export class AppModule { }
